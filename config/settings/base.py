46dc69c65a20e4aa541fb6bcdf47ba615662a2fa
정유진 <jyj_1011@naver.com>
2023-09-17 19:32:02 +0900

    수정내용

--- config/settings/base.py
+++ config/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^e0vvd-cu_koc%d)m!f$c*$s4ufsi1!q@)lyz56p7_&vpmzyt4
 # SECURITY WARNINSTATICFILES_DIRS = []G: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.125.30.103']
 
 
 # Application definition
